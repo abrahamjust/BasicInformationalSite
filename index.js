be5c023cded090596d7a19601213785bd53ddcbf
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2026-06-19 09:12:45 +0000

    created basic project

---
+++ index.js
@@ -0,0 +1,50 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(fs.readFileSync('index.html'));
+//     } else if (req.url === '/about') {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(fs.readFileSync('about.html'));
+//     } else if (req.url === '/contact-me') {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(fs.readFileSync('contact-me.html'));
+//     } else {
+//         res.writeHead(200, { 'Content-Type': 'text/html'});
+//         res.end(fs.readFileSync('404.html'));
+//     }
+// });
+
+// server.listen(8080);
+
+const server = http.createServer((req, res) => {
+    let url = req.url;
+    let filePath = "";
+
+    if (url === '/') {
+        filePath = "index.html";
+    } else if (url === '/about') {
+        filePath = "about.html";
+    } else if (url === '/contact-me') {
+        filePath = "contact-me.html";
+    } else {
+        filePath = "404.html";
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal error - 500");
+        } else {
+            let statusCode = (filePath === '404.html') ? 404 : 200;
+            res.writeHead(statusCode, { "Content-Type": "text/html" });
+            res.end(content, "utf-8");
+        }
+    });
+});
+
+server.listen(8080, () => {
+    console.log("server running on port 8080");
+});
